21724c84169181503c7ee0b4b28a17998c184b8f
Maxwell <lubalecliff@gmail.com>
2023-10-17 20:51:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = { name, email };
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+    return fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        document.body.innerHTML += data.id;
+      })
+      .catch(error => {
+        document.body.innerHTML += error.message;
+      });
+  }
+
